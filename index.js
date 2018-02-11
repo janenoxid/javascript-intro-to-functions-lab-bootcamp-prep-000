c3eefad1c87968eb7dbbb9f782f1b17b9469a4b4
Desiree Jane Dixon <desireejanedixon@gmail.com>
2018-02-11 23:53:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function sayHiToGrandma(string)
   return "I can\'t hear you!"
 }else if (string === string.toUpperCase()){
   return "YES INDEED!"
-}else if (string === "I love you, Grandma"){
+}else if (string === "I love you, Grandma."){
   return "I love you, too."
-}string === "I love you, Grandma"
+}
 }
   
